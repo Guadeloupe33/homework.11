39a3e043446f162472aafb5f0e03dd8938bf52dc
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-14 14:23:38 -0400

    updated the read me

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,44 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware to handle JSON data
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-// Use your API and HTML routes
-app.use(require('./routes/apiRoutes'));
-app.use(require('./routes/htmlRoutes'));
+// Define an array to hold notes (you can replace this with your database logic)
+let notes = [];
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = notes.length + 1; // Assign a unique ID (you may use a library for this)
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes), 'utf8');
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const idToDelete = parseInt(req.params.id);
+  notes = notes.filter((note) => note.id !== idToDelete);
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes), 'utf8');
+  res.json({ message: 'Note deleted' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # homework.11
+
+
+This is a simple note-taking web application built with Node.js, HTML, CSS, and JavaScript. It allows users to create, view, and save notes.
+
+## Features
+
+- **Landing Page:** The landing page provides a quick link to access your notes.
+
+- **Notes Page:** The notes page displays existing notes on the left and an input form on the right to create new notes.
+
+- **Create a New Note:** You can create a new note by providing a title and text in the input form and clicking the "Save Note" button.
+
+- **View and Edit Notes:** Clicking on an existing note in the list on the left-hand side will display its details on the right. You can then edit and update the note.
+
+- **Clear Form:** The "delete" button clears the input fields for creating a new note.
+
+## Getting Started
+
+1. **Prerequisites:** Ensure you have Node.js installed on your computer.
+
+2. **Clone the Repository:**
