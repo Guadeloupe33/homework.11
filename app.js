870381822923c63f4f797dc04c3d8f1c51c288ac
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-14 12:39:22 -0400

    added js to the file

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/views/notes.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
