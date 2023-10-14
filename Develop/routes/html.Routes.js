3d7216f1959590c65159ea4e8f8974a1d3a87edc
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-14 13:31:42 -0400

    added html routes

---
+++ Develop/routes/html.Routes.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+// GET /notes - Return the notes.html file
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// GET * - Return the index.html file
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
