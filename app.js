103b9a900f76bed3961770ef45497651998602a9
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-14 12:38:39 -0400

    added server js to the file

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
