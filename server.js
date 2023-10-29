abec6ea0a24be5b0e5ad64b19776fe3e0d374043
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-28 20:45:30 -0400

    added addtional funactionlity

---
+++ server.js
@@ -0,0 +1,22 @@
+// require dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+// initialize
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// data expressions
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+//include the routes file
+require('./routes/routes')(app);
+
+//create listener
+app.listen(PORT, function() {
+    console.log("Listening on Port: " + PORT);
+});
